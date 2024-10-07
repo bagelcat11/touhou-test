983b92f39149574344e6f98c3697c51f83e90e87
Chrysa Nguyen <chrysaliszt@gmail.com>
2024-10-06 22:29:52 -0700

    Bullet test: color and rotation
    Tested some ways to change bullet color and angle.

--- scripts/player_bullet.gd
+++ scripts/player_bullet.gd
@@ -1,11 +1,24 @@
 extends Area2D
 
-@export var speed = 30
+@onready var sprite = $sprite
+@export var speed = 20
+@export var rotSpeed = 4
 var damage
+var color = Color.from_hsv(1, 0.5, 1)
 
 
 func _physics_process(_delta):
+	if(Input.is_key_pressed(KEY_O)):
+		speed = 0
+		rotSpeed = 0
+	if(Input.is_key_pressed(KEY_P)):
+		speed = 20
+		rotSpeed = 4
 	position += transform.x * speed # * delta
+	rotation += deg_to_rad(rotSpeed)
+	color.h += 0.01 
+	sprite.modulate = color
+	
 
 
 func _on_area_entered(area: Node2D) -> void:
