38a8140ec52307f81da8566aa3aa11ed267f40c8
bagelcat11 <lynn.g0032@gmail.com>
2024-10-12 02:16:33 -0700

    added credits page

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,8 +3,9 @@ extends Control
 #@onready var main_game = preload("res://scenes/main_game.tscn")
 
 # Called when the node enters the scene tree for the first time.
-#func _ready() -> void:
-	#pass # Replace with function body.
+func _ready() -> void:
+	$cred_bg.hide()
+	$back.hide()
 #
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -22,3 +23,19 @@ func _on_quit_pressed() -> void:
 func _process(delta: float) -> void:
 	if(Input.is_key_pressed(KEY_Z)):
 		get_tree().change_scene_to_file("res://scenes/main_game.tscn")
+
+
+func _on_creds_pressed() -> void:
+	$cred_bg.show()
+	$start.hide()
+	$creds.hide()
+	$quit.hide()
+	$back.show()
+
+
+func _on_back_pressed() -> void:
+	$cred_bg.hide()
+	$start.show()
+	$creds.show()
+	$quit.show()
+	$back.hide()
