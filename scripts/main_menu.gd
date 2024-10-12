fb2f8d8e8cf9bf6b9dc95a8d92e96bfec974060c
spear <62441082+workingSpear@users.noreply.github.com>
2024-10-12 16:57:58 -0700

    Implemented Button Sprites
    touhou touhou
    touhou touhou
    purojekto
    purojekto

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$cred_bg.hide()
-	$back.hide()
+	$Back.hide()
 #
 #
 ## Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,38 +13,22 @@ func _ready() -> void:
 	#pass
 
 
-func _on_start_pressed() -> void:
-	$music_fader.play("fade_out_music")
-	$sfx_ui.play()
-	await get_tree().create_timer(0.5).timeout # lmao
-	get_tree().change_scene_to_file("res://scenes/main_game.tscn")
 
 
-func _on_quit_pressed() -> void:
-	$sfx_ui.play()
-	GlobalVars.exit()
+
 
 func _process(delta: float) -> void:
 	if(Input.is_key_pressed(KEY_Z)):
 		get_tree().change_scene_to_file("res://scenes/main_game.tscn")
 
 
-func _on_creds_pressed() -> void:
-	$sfx_ui.play()
-	$cred_bg.show()
-	$start.hide()
-	$creds.hide()
-	$quit.hide()
-	$back.show()
-
-
 func _on_back_pressed() -> void:
 	$sfx_ui.play()
 	$cred_bg.hide()
-	$start.show()
-	$creds.show()
-	$quit.show()
-	$back.hide()
+	$Starts.show()
+	$Credits.show()
+	$Quit.show()
+	$Back.hide()
 
 
 
@@ -57,3 +41,16 @@ func _on_start_2_button_up() -> void:
 func _on_music_ui_finished() -> void:
 	$music_ui.play()
 
+
+func _on_credits_button_up() -> void:
+	$sfx_ui.play()
+	$cred_bg.show()
+	$Starts.hide()
+	$Credits.hide()
+	$Quit.hide()
+	$Back.show()
+
+
+func _on_quit_button_up() -> void:
+	$sfx_ui.play()
+	GlobalVars.exit()
